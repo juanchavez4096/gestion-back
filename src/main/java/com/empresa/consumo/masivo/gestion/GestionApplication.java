01b89c8f2eb383f453cd1ec4517ea6930497772a
Juan Jose Chávez Huerta <jjchavez@urbe.edu.ve>
2018-12-28 10:37:00 -0400

    first commit

---
+++ src/main/java/com/empresa/consumo/masivo/gestion/GestionApplication.java
@@ -0,0 +1,14 @@
+package com.empresa.consumo.masivo.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionApplication.class, args);
+	}
+
+}
+
